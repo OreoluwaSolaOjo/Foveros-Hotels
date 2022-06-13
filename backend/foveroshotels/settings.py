a0937bc1847b59e5d7b3f23525924b471782f7be
Oreoluwa Sola-Ojo <53820588+OreoluwaSolaOjo@users.noreply.github.com>
2022-06-13 08:25:41 -0500

    Updatedd

--- backend/foveroshotels/settings.py
+++ backend/foveroshotels/settings.py
@@ -119,12 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # apppassword: 'fxnqaxdpggmihedh'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'oreoluwasolaojo@gmail.com'
-EMAIL_HOST_PASSWORD = 'fxnqaxdpggmihedh'
-EMAIL_USE_TLS = True
+
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
